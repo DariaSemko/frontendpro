7ec4c7ede9b0e81f4343dfaa0f12c6abd85cc679
Daria Semko <darinakula@MacBook-Pro-Daria.local>
2023-05-16 20:00:54 +0300

    hw-28

---
+++ Home work/hw-28/my-app/src/ui/components/caption.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Caption extends React.Component {
+    render() {
+        return (
+            <header className="header">
+                <h1>Click on smile</h1>
+            </header>
+        )
+    }
+}
---
+++ Home work/hw-28/my-app/src/ui/pages/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import '../../style.css';
+import Caption from "../components/caption";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.showResults = this.showResults.bind(this)
+        this.smiles = [
+            {id: 1, name: 'Smile 1', count: 0},
+            {id: 2, name: 'Smile 2', count: 0},
+            {id: 3, name: 'Smile 3', count: 0},
+            {id: 4, name: 'Smile 4', count: 0},
+            {id: 5, name: 'Smile 5', count: 0},
+
+        ];
+    }
+
+    showResults() {
+        let winner = this.smiles.reduce((prev, current) => (prev.count > current.count ? prev : current));
+        const winnerElement = document.querySelector('.winner');
+        winnerElement.innerHTML = `<h2>Winner: ${winner.name} (${winner.count} votes)</h2>`;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Caption/>
+                <main className='main'>
+                    <div className="image">
+                        <img alt='smile'
+                            src='https://symbl-world.akamaized.net/i/webp/4f/070ccbbb696c2975bb0da521c2ffc5.webp'
+                               onClick={() => {
+                                   this.smiles[0].count += 1
+                               }
+                               }
+                        />
+                        <img alt='smile'
+                            src='https://symbl-world.akamaized.net/i/webp/55/ceb7ce388c8b07ffa8495e9d8905bd.webp'
+                            onClick={() => {
+                                this.smiles[1].count += 1
+                            }
+                            }
+                        />
+                        <img alt='smile'
+                            src='https://symbl-world.akamaized.net/i/webp/27/f2d2f72f7e79b792bcb6f1019253ba.webp'
+                            onClick={() => {
+                                this.smiles[2].count += 1
+                            }
+                            }
+                        />
+                        <img alt='smile'
+                            src='https://symbl-world.akamaized.net/i/webp/b6/006751651eeab3a5d8de403cc6bce0.webp'
+                            onClick={() => {
+                                this.smiles[3].count += 1
+                            }
+                            }
+                        />
+                        <img alt='smile'
+                            src='https://symbl-world.akamaized.net/i/webp/fd/dd5b841cf3eac8cbaa636a586f3c19.webp'
+                            onClick={() => {
+                                this.smiles[4].count += 1
+                            }
+                            }
+                        />
+                    </div>
+                    <button className='btn' onClick={() => {
+                        this.showResults()
+                    }
+                    }>Show results
+                    </button>
+                    <div className='winner'>
+
+                    </div>
+                </main>
+            </div>
+        );
+    }
+}
+export default App;
